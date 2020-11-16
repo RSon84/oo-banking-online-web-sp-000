c8705c60b8048a5da224a1bfebd354b61041d468
Devin Wilson <craigvin84@yahoo.com>
2020-11-16 19:59:36 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -30,16 +30,16 @@ end
 
 
 def execute_transaction
-    if
-      @sender.valid? == false || @sender.balance < amount || @status != "pending"
-      @status = "rejected"
-      return "Transaction rejected. Please check your account balance."
+  if @sender.balance < @amount
+    @status = "rejected"
+    return "Transaction rejected. Please check your account balance."
+
+  elsif @status == "complete"
+    puts "Transaction was already excuted"
   else
-      @sender.balance > amount && @status == "pending" && @sender.valid? == true
-      @sender.balance -= amount
-      @receiver.balance += amount
-      @status = "complete"
+    @sender.deposit( @amount * -1 ) 
+    @receiver.deposit( @amount )
+    @status = "complete"
   end
 end
-
 end
