047432df0a73f721f2626e8bde6d157f20d0aa33
Devin Wilson <craigvin84@yahoo.com>
2020-11-16 21:26:21 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -15,26 +15,26 @@ end
 
 
 def execute_transaction
-  if @sender.balance > @amount && valid? && self.status == "pending"
+  if @sender.balance > @amount && valid? && @status == "pending"
 
         @sender.balance -= @amount
         @receiver.balance += @amount
-    self.status = "complete"
+    @status = "complete"
   else
     bad_transfer
   end
 end
 
 def bad_transfer
-  self.status = "rejected"
+  @status = "rejected"
   return "Transaction rejected. Please check your account balance."
 end
 
 def reverse_transfer
-  if self.status = "complete"
+  if @status = "complete"
   @sender.balance += @amount
   @receiver.balance -= @amount
-  self.status = "reversed"
+  @.status = "reversed"
 
 end
 end
