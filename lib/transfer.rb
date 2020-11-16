774b36085be7f429a11d23e042abc671c9176724
Devin Wilson <craigvin84@yahoo.com>
2020-11-16 19:58:17 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,15 +14,31 @@ class Transfer
     @sender.valid? && @receiver.valid? ? true : false
 end
 
+# def execute_transaction
+#   if @sender.balance > amount && @status == "pending" && @sender.valid? == true
+#     @sender.balance -= amount
+#     @receiver.balance += amount
+#     @status = "complete"
+#   else
+#     @sender.valid? == false || @sender.balance < amount || @status != "pending"
+#     @status = "rejected"
+#     return "Transaction rejected. Please check your account balance."
+#   end
+# end
+# 
+# end
+
+
 def execute_transaction
-  if @sender.balance > amount && @status == "pending" && @sender.valid? == true
-    @sender.balance -= amount
-    @receiver.balance += amount
-    @status = "complete"
+    if   
+      @sender.valid? == false || @sender.balance < amount || @status != "pending"
+      @status = "rejected"
+      return "Transaction rejected. Please check your account balance."
   else
-    @sender.valid? == false || @sender.balance < amount || @status != "pending"
-    @status = "rejected"
-    return "Transaction rejected. Please check your account balance."
+      @sender.balance > amount && @status == "pending" && @sender.valid? == true
+      @sender.balance -= amount
+      @receiver.balance += amount
+      @status = "complete"
   end
 end
 
