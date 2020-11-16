5fbe357a8cee075e5fdd19cad2641dd505d8d15a
Devin Wilson <craigvin84@yahoo.com>
2020-11-16 19:58:21 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -25,12 +25,12 @@ end
 #     return "Transaction rejected. Please check your account balance."
 #   end
 # end
-# 
+#
 # end
 
 
 def execute_transaction
-    if   
+    if
       @sender.valid? == false || @sender.balance < amount || @status != "pending"
       @status = "rejected"
       return "Transaction rejected. Please check your account balance."
